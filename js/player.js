2931f1a64b8e1371f7f34e95cae26f11f8c306cf
distantplate <philiphslade478@gmail.com>
2026-04-17 08:29:19 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -6054,6 +6054,24 @@ const m = {
                             m.pickUp();
                         } else {
                             m.holdingTarget = null; //clears holding target (this is so you only pick up right after the field button is released and a hold target exists)
+                            if ((m.lanceCharge && !tech.isPlasmaLance) || (tech.isPlasmaLance && !(m.lanceCharge >= 0))) {
+                                m.lanceCharge = 0;
+                            } else if (tech.isPlasmaLance && m.lanceCharge < 0.2 && m.energy >= m.maxEnergy) {
+                                m.lanceCharge += 0.00075;
+                            } else if (tech.isPlasmaLance && m.lanceCharge > 0.2) {
+                                m.lanceCharge = 0.2;
+                            }
+                        }
+                        if (tech.isPlasmaLance && m.lanceCharge > 0) {
+                            ctx.save();
+                            ctx.beginPath();
+                            ctx.lineWidth = 5;
+                            let chargeLevel = m.lanceCharge * 1275;
+                            let colorVal = (tech.isPlasmaLance ? "rgb(255,".concat(Math.min(136,chargeLevel),",",(255-Math.min((chargeLevel*2),255)),")") : "rgb(255,0,255)");
+                            ctx.strokeStyle = colorVal;
+                            ctx.arc(m.pos.x, m.pos.y, 4.2*Math.sqrt(50), m.angle, (m.lanceCharge*10*Math.PI)+m.angle);
+                            ctx.stroke();
+                            ctx.restore();
                         }
                         m.drawRegenEnergy("rgba(0, 0, 0, 0.2)")
                     }
