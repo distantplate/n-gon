8e8e6c3f38bef3b91fb122be5c15849a15e0ecfc
distantplate <philiphslade478@gmail.com>
2026-04-20 10:08:48 -0400

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -2409,7 +2409,7 @@ const b = {
             let supercharge = (tech.isPlasmaSupercharge ? 4 : 1);
             let chargeLevel = m.lanceCharge*1275;
             let colorVal = (tech.isPlasmaSupercharge && m.lanceCharge ? "rgba(".concat("255,",Math.min(136,chargeLevel),",",(255-Math.min((chargeLevel*2),255)),",") : "rgba(255,0,255,");
-            m.energy -= DRAIN;
+            m.energy -= DRAIN * ((tech.isPlasmaRadiation && m.crouch) ? 2 : 1);
             let chargeScale = (m.lanceCharge ? Math.min(m.lanceCharge*5*supercharge,2) : 0);
             if (m.energy < 0) {
                 m.fieldCDcycle = m.cycle + 120;
@@ -2439,7 +2439,7 @@ const b = {
                 if (best.who.alive) {
                     const dmg = 0.9; //********** SCALE DAMAGE HERE *********************
                     best.who.damage(dmg*(1+chargeScale));
-                    if (tech.isPlasmaRadiation) {mobs.statusDoT(best.who, 1, 60);}
+                    if (tech.isPlasmaRadiation && m.crouch) {mobs.statusDoT(best.who, 1, 60);}
                     best.who.locatePlayer();
 
                     //push mobs away
